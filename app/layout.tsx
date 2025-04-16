6419c9db9a7b5e037c0cbda68f6ca5a664424da7
EnterRyan <changetodev@gmail.com>
2025-04-16 23:17:28 +0900

    레이아웃 구조 재수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ import './globals.css'
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="ko">
-     <body className="grid-layout-main">
-      <div className="area-header"><Header /></div>
+     <body className="grid-layout-main h-screen">
+      <div className="area-header h-1"><Header /></div>
       <div className="area-side"><SideNavigator /></div>
       <main className="area-contents">{children}</main>
       <footer className="area-footer"><Footer /></footer>
