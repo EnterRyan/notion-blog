aca981ed2a4f5a9d5aac60c9cb1f385d1cbefe93
EnterRyan <changetodev@gmail.com>
2025-04-23 18:14:42 +0900

    스토리북 설정 변경 및 PostCard 컴포넌트 등록

--- src/entities/client/postCard/ui/PostCard.tsx
+++ src/entities/client/postCard/ui/PostCard.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
+import Image from 'next/image';
 import { Post } from "@entities-server/post/type/postListType";
 
 export default function PostCard({title,group,createDate,pageId,state,tags, thumbnail}:Post){
   return (
     <Link href={`/post/${pageId}`} >
+      <Image src={thumbnail} alt="썸네일" width={400} height={220} className="rounded-lg object-cover"/>
       <div className="">
         <p>{title}</p>
         <p>{group}</p>
         <p>{createDate}</p>
         <p>{state}</p>
         <p>{JSON.stringify(tags)}</p>
-        <p>{pageId}</p>
-        <p>{thumbnail}</p>
       </div>
     </Link>
   )
