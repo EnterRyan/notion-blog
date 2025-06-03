dbad20250c499b42164ced7c9c6c61ae28d20b6d
TaeHunKim <changetodev@gmail.com>
2025-06-04 02:55:48 +0900

    CSR 컴포넌트를 위한 api 구조설계

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,19 @@ import { Header } from '@widgets/header';
 import { Footer } from '@widgets/footer';
 
 import { SideNavigator } from '@widgets/sideNavigator';
+import ReactQueryProvider from '@shared-client/providers/ReactQueryProvider';
 import './globals.css'
 
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="ko">
       <body className="grid-layout-main h-screen">
-        <div className="area-header"><Header /></div>
-        <div className="area-side"><SideNavigator /></div>
-        <main className="area-contents">{children}</main>
-        <footer className="area-footer"><Footer /></footer>
+        <ReactQueryProvider>
+          <div className="area-header"><Header /></div>
+          <div className="area-side"><SideNavigator /></div>
+          <main className="area-contents">{children}</main>
+          <footer className="area-footer"><Footer /></footer>
+        </ReactQueryProvider>
       </body>
     </html>
   );
