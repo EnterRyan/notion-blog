a08cc2ac0757085f16e14acbaa1733649c47a5a8
EnterRyan <changetodev@gmail.com>
2025-06-17 16:05:05 +0900

    api 테스트

--- src/features/server/post/model/getNotionPageData.ts
+++ src/features/server/post/model/getNotionPageData.ts
@@ -1,13 +1,21 @@
 import { NotionAPI } from 'notion-client';
 
 export async function getNotionPageData(notionPageId: string) {
-  const notionInstance = new NotionAPI();
+  if (!process.env.NOTION_API_KET) {
+    throw new Error('NOTION_TOKEN is not defined');
+  }
+
+  const notionInstance = new NotionAPI({
+    authToken: process.env.NOTION_API_KET,
+  });
+
   try {
+    console.log('[getNotionPageData] Requesting page:', notionPageId);
     const response = await notionInstance.getPage(notionPageId);
     console.log('[getNotionPageData] Success:', response);
     return response;
   } catch (error) {
-    console.error('[getNotionPageData]', JSON.stringify(error, null, 2));
+    console.error('[getNotionPageData] Error:', JSON.stringify(error, null, 2));
     throw error;
   }
 }
--- src/features/server/post/ui/ViewPostPage.tsx
+++ src/features/server/post/ui/ViewPostPage.tsx
@@ -1,11 +1,13 @@
 
 import { PostViewer } from "@entities/notionReder";
 import { getNotionPageData } from "../model/getNotionPageData";
-
+import { NotionAPI } from "notion-client";
 
 export default async function ViewPostPage({ pageId }: { pageId: string }){
-  const recordMap = await getNotionPageData(pageId);
-  
+  // const recordMap = await getNotionPageData(pageId);
+  const notion = new NotionAPI()
+  const recordMap = await notion.getPage(pageId);
+
   if(!recordMap){
     console.error('❌ Notion 데이터 불러오기 실패:', pageId);
     return (
