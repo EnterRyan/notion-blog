008e30d20476a8c8ff785f0ace91e111e96e196b
EnterRyan <changetodev@gmail.com>
2025-06-24 16:21:24 +0900

    vercel analytics 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next"
+import { Analytics } from "@vercel/analytics/next"
 
 import { Header } from '@widgets/header';
 import { Footer } from '@widgets/footer';
--- src/widgets/sideNavigator/ui/SideNavigator.tsx
+++ src/widgets/sideNavigator/ui/SideNavigator.tsx
@@ -8,9 +8,9 @@ export default async function SideNavigator(){
       <div className="w-full flex flex-col items-center gap-4">
         <Avatar src="/test.jpg" alt="프로필 이미지" size={230} />
         <div className="p-4 rounded-xl shadow bg-white dark:bg-gray-800 border border-gray-300 max-w-xs w-full transition-colors">
-          <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-1">FE Dev Blog</h2>
+          <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-1">ChangeTodev Blog</h2>
           <p className="text-sm text-gray-700 dark:text-gray-300">
-            프론트엔드 개발자 <b>김태헌</b>입니다.<br/>기술 포스팅, 개인 프로젝트, 학습내용 과 잡담을 올리고 있는 블로그입니다.
+            프론트엔드 개발자 <b>김태헌</b>입니다.<br/>기술 포스팅, 개인 프로젝트, 학습내용 과 사담을 올리고 있는 블로그입니다.
           </p>
         </div>
         <SearchForm />
