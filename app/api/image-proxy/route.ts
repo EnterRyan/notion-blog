b9793d1a58c91709e384cd2f15fb875ccf199d75
EnterRyan <changetodev@gmail.com>
2025-06-27 01:05:38 +0900

    이미지 프록시 선언 변경

--- app/api/image-proxy/route.ts
+++ app/api/image-proxy/route.ts
@@ -15,7 +15,7 @@ export async function GET(req: NextRequest) {
         'Cache-Control': 'public, max-age=86400', // 캐시 하루
       },
     })
-  } catch (e) {
+  } catch {
     return new NextResponse('Failed to fetch image', { status: 500 })
   }
 }
