9b818ee4fa710e5624b72edcff22b74c8c715873
EnterRyan <changetodev@gmail.com>
2025-07-01 00:10:54 +0900

    무한스크롤 구현 처리완료

--- src/features/client/postInfiniteScroll/model/getInfinitePostList.ts
+++ src/features/client/postInfiniteScroll/model/getInfinitePostList.ts
@@ -1,18 +1,21 @@
-
-type GetInfinitePostListType = {category : string,pageSize : number, cursor:string};
 export async function getInfinitePostList(
   category:string, 
   pageSize:number,
-  cursor?:string
-  ){
-  // 쿼리파라미터로 undefined 보내기  
-  const baseUrl = new URL( `/api/postsList`, window.location.origin);
-  baseUrl.searchParams.set("category", category);
-  baseUrl.searchParams.set("pageSize", pageSize.toString());
-  if (cursor) { baseUrl.searchParams.set("cursor", cursor);}
-  
-  const res = await fetch(baseUrl);
-  if (!res.ok) throw new Error('에러 발생!');
-  const data =await res.json();
-  return data;
+  cursor?:string){
+    console.log("called");
+    // 쿼리파라미터로 undefined 보내기  
+    const baseUrl = new URL( `/api/postsList`, window.location.origin);
+    baseUrl.searchParams.set("category", category);
+    baseUrl.searchParams.set("pageSize", pageSize.toString());
+    if (cursor) { baseUrl.searchParams.set("cursor", cursor);}
+    
+    const res = await fetch(baseUrl);
+    if (!res.ok) throw new Error('에러 발생!');
+    const data =await res.json();
+
+    return {
+      postList : data.postList,
+      nextCursor: data.nextCursor,
+      hasMore: data.hasMore
+    }
 }
--- src/features/client/postInfiniteScroll/ui/infiniteScrollPostCard.tsx
+++ src/features/client/postInfiniteScroll/ui/infiniteScrollPostCard.tsx
@@ -1,41 +1,64 @@
 'use client'
-import { useEffect, useState } from "react";
-import { getInfinitePostList } from "../model/getInfinitePostList";
+import { useEffect, useRef, useState } from "react";
+import { useInfiniteQuery } from '@tanstack/react-query';
+
 import { PostList } from "@shared-common/types";
 import { PostCard } from "@entities/postCard";
 
-type InfiniteScrollPostCardType = {
-  category : string;
-}
+import { getInfinitePostList } from "../model/getInfinitePostList";
+
 const PAGE_SIZE = 6;
+type InfiniteScrollPostCardType = {category : string;}
 export default function InfiniteScrollPostCard({category}:InfiniteScrollPostCardType){
   const [renderPostList, setRenderPostList] = useState<PostList>([]);
+  //인식이 되면 쿼리가 콜될 div 지정
+  const observerRef = useRef<HTMLDivElement | null>(null);
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey : ['postList', category],
+    queryFn : ({pageParam=''})=> getInfinitePostList(category,PAGE_SIZE, pageParam ),
+    initialPageParam: '',
+    getNextPageParam: (lastPage) => lastPage.hasMore ? lastPage.nextCursor : undefined,
+  });
 
   useEffect(()=>{
-    const fetchPostList= async ()=>{
-      const {postList,nextCursor, hasMore} = await getInfinitePostList(category,PAGE_SIZE,"");
-      setRenderPostList(postList);
-    }
-    fetchPostList();
-  },[]);
+    const observer = new IntersectionObserver((entries)=>{
+      if(entries[0].isIntersecting && hasNextPage && !isFetchingNextPage){
+        fetchNextPage();
+      }
+    }, { threshold: 0.5, root: null });
+
+    if (observerRef.current) observer.observe(observerRef.current);
+    return () => observer.disconnect();
+  },[hasNextPage, isFetchingNextPage, fetchNextPage]);
+
   return(
     <div className="w-full h-full p-2 sm:p-[30px]">
       <div className="pb-[3rem] overflow-y-scroll overflow-x-hidden h-full flex flex-wrap justify-center gap-5 mx-auto">
-      {renderPostList.map((item)=>(
-        <div className="post-card-wrapper">
-          <PostCard 
-            key={item.pageId}
-            pageId={item.pageId}
-            createDate={item.createDate}
-            group={item.group}
-            state={item.state}
-            tags={item.tags}
-            title={item.title}
-            thumbnail={item.thumbnail}
-          />
-        </div>
+      {data?.pages.map((page)=>(
+        page.postList.map((item: any) => (
+          <div className="post-card-wrapper" key={item.pageId}>
+            <PostCard 
+              key={item.pageId}
+              pageId={item.pageId}
+              createDate={item.createDate}
+              group={item.group}
+              state={item.state}
+              tags={item.tags}
+              title={item.title}
+              thumbnail={item.thumbnail}
+            />
+          </div>
+        ))
         ))}
-        <div className="w-full">옵저버 div에용</div>
+        <div ref={observerRef} className="w-full text-center text-sm text-gray-400">
+          {isFetchingNextPage ? '불러오는 중...' : '모든 데이터를 불러왔습니다.'}
+          </div>
       </div>
     </div>
   )
