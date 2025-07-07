0331357f3305acb4491f35ac5ae8f5f9b03d10ff
EnterRyan <changetodev@gmail.com>
2025-07-07 10:55:05 +0900

    사이드 네비게이션 스크롤링 처리

--- app/api/postsList/route.ts
+++ app/api/postsList/route.ts
@@ -5,6 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(req: NextRequest){
   const {searchParams} = new URL(req.url);
   const category = searchParams.get('category');
+  // 팁: parseInt는 명시적으로 10진수로 인식하라고 선언 해야함 ,10
   const pageSize = parseInt(searchParams.get('pageSize')||'6',10);
   const startCursor = searchParams.get('cursor') ??undefined;
 
@@ -13,7 +14,6 @@ export async function GET(req: NextRequest){
 
   if (category && validCategories.includes(category as Category)) {
     const safeCategory = category as Category;
-    // 팁: parseInt는 명시적으로 10진수로 인식하라고 선언 해야함 ,10
     const {postList, nextCursor, hasMore} = await GetPostList(safeCategory, pageSize, startCursor);
     
     return NextResponse.json(
--- src/widgets/sideNavigator/ui/SideNavigator.tsx
+++ src/widgets/sideNavigator/ui/SideNavigator.tsx
@@ -4,9 +4,9 @@ import SearchForm from '@shared-common/components/searchForm/ui/searchForm';
 
 export default async function SideNavigator(){
   return(
-    <nav className='w-[250px] h-full m-3'>
+    <nav className='w-[260px] h-full m-3'>
       <div className="w-full flex flex-col items-center gap-4">
-        <Avatar src="/test.jpg" alt="프로필 이미지" size={230} />
+        <Avatar src="/test.jpg" alt="프로필 이미지" size={180} />
         <div className="p-4 rounded-xl shadow bg-white dark:bg-gray-800 border border-gray-300 max-w-xs w-full transition-colors">
           <h2 className="text-lg font-bold text-gray-900 dark:text-white mb-1">ChangeTodev Blog</h2>
           <p className="text-sm text-gray-700 dark:text-gray-300">
@@ -14,9 +14,9 @@ export default async function SideNavigator(){
           </p>
         </div>
         <SearchForm />
-        <div className="flex flex-col w-full">
-          <TagSummary />
-        </div>
+      </div>
+      <div className="flex flex-col w-full max-h-[300px] mt-4">
+        <TagSummary />
       </div>
     </nav>
   ) 
